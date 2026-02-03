d8a089cb797a7dffa42f3176e02654ce0dc6c0b3
PCNICOLA\nicol <nicola.bonfanti@gmail.com>
2026-02-03 14:26:26 +0100

    Classe car che rappresenta le macchine del gioco

---
+++ Car.java
@@ -0,0 +1,56 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Car extends Thread {
+
+    private int posX, posY;
+    private int W, H;
+    private int velocita = 200; 
+    private Color color = Color.RED;
+    private boolean running = true;
+    private int panelWidth = 500; 
+
+    public Car(int posX, int posY, int W, int H, int panelWidth) {
+        this.posX = posX;
+        this.posY = posY;
+        this.W = W;
+        this.H = H;
+        this.panelWidth = panelWidth;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(posX, posY, W, H);
+        g.setColor(Color.BLACK);
+        g.drawRect(posX, posY, W, H);
+    }
+
+
+    public void stopCar() {
+        running = false;
+    }
+
+    @Override
+public void run() {
+    while (running) {
+        posX += 5; 
+
+        if (posX > panelWidth) {
+            posX = -W;
+        }
+
+        try {
+            sleep(velocita); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+    // Getters
+    public int getPosX() { return posX; }
+    public int getPosY() { return posY; }
+    public int getW() { return W; }
+    public int getH() { return H; }
+}
