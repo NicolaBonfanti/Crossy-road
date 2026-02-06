1ba372d9adfe0d46fa091a96cd18e72996124219
PCNICOLA\nicol <nicola.bonfanti@gmail.com>
2026-02-06 14:48:37 +0100

    creazione delle classi prato e strada la prima gestisce la creazione di posti sicuri la seconda gestisce la creazione della strada (singola corsia) con all'interno macchine create in posizioni casuali e controllo delle collisioni

---
+++ Prato.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Prato extends Lane{
+    private Color colorGround = new Color(34, 139, 34);
+
+    public Prato(int y){ 
+        super(y);
+    }
+    
+    @Override
+    public void draw(Graphics g)
+    {
+        g.setColor(colorGround);
+        g.fillRect(0,y,500,40);
+    }
+    
+}
---
+++ Strada.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class Strada extends Lane{
+    private ArrayList<Car> cars;
+    private Color colorStreet = new Color(77, 77, 77);
+    private Random random = new Random();
+
+    public Strada(int y, int velocita, int nCars) {
+        super(y);
+        this.cars = new ArrayList<>();
+
+        int ultimaX = 0;
+        //creazione macchine
+        for(int i = 0; i < nCars; i++){
+            //creazione della macchina in posizione x casuale
+            int spazio = 150 + random.nextInt(200);
+            int x = ultimaX + spazio;
+            Car c = new Car(x,y+5,80,30);
+
+            cars.add(c);
+            c.start();
+
+            ultimaX = x;
+        }
+    }
+
+
+        @Override
+        public void draw(Graphics g) {
+            g.setColor(colorStreet);
+            g.fillRect(0, y, 500, 40);
+
+            for(Car c : cars)
+                c.draw(g);
+        }
+
+
+        public boolean checkCollisioni(pollo p){
+            //controllo se si trova nella corsia
+            if(p.getPosY() < y || p.getPosY() >= y+40)
+                return false;
+
+            //controllo se collide con un'auto
+            for (Car c : cars) {
+                boolean collisioneX = (p.getPosX() + p.getW() > c.getPosX()) && (p.getPosX() < c.getPosX() + c.getW());
+                boolean collisioneY = (p.getPosY() + p.getH() > c.getPosY()) && (p.getPosY() < c.getPosY() + c.getH());
+
+                if (collisioneX && collisioneY) 
+                    return true; 
+            }
+            return false;
+        }
+
+
+        public void reset(){
+            //ferma i thread
+            for(Car c : cars)
+                try {
+                    c.join();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            
+            //svuota la lista di macchine
+            cars.clear();
+        }
+}
